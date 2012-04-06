f169c523deb0c1c3640c52038cf0a97a683a50ac
Alex <alex@Scar.(none)>
2012-04-06 12:18:12 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buddybalance::Application.config.secret_token = '767a3ba5e13a1a47dffbfff8c2d141d190fb48b29b023faf626281d7f461b73d71bd93e16db412a8aba88a61eefc2ee145f4e55ead6f5fd8e1638a71a0265cc4'
